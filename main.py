e0fd82a20ccd2ba46a72417be1d1557ec6e3488f
Ryan Curry <rtc1995@yahoo.com>
2017-01-23 23:17:20 -0600

    added random lucky number and random fortune

--- main.py
+++ main.py
@@ -15,11 +15,40 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+
+
+def GetRandomFortune():
+    FortuneList=["There is a cake waiting for you","there is much code in your future","you need to eat more Fortune Cookies"]
+
+
+    return FortuneList[random.randint(0,len(FortuneList)-1)]
+    #for i in
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        luckynumber= "<strong>" + str(random.randint(1,100)) + "</strong>"
+        header="<h1>Fortune Cookie</h1>"
+        num='Your lucky number is ' + luckynumber
+        nump="<p>"+num+"</p>"
+        button="</br><a href='.'><button>another cookie plz!</button></a>"
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+        Fortune="<strong>" + GetRandomFortune() + "</strong>"
+        FortuneP="<p> Your Fortune is: "  + Fortune + "</p>"
+
+        self.response.write(header+num+FortuneP+button)
+
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("thanks for loging in")
+
+
+pages=[
+    ('/', MainHandler),
+    ('/login', LoginHandler)
+]
+
+
+app = webapp2.WSGIApplication(pages, debug=True)
